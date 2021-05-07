d6448696d481c1a5318cbdfe660ae2decd093dde
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2021-05-07 17:40:31 +0530

    add_email_with_sendinblue

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+
+  :address => "smtp-relay.sendinblue.com",
+  :port => 587,
+  :user_name => ENV['SEND_IN_BLUE_USER_NAME'],
+  :password => ENV['SEND_IN_BLUE_PASSWORD'],
+  :authentication => "login",
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+}
